56e7601b6fc3f2c4a0fd5a52ef3b156cad3d3dff
Варя Лиханина <varyasimac@iMac-Vara.local>
2020-09-30 21:57:57 +0300

    Updates

---
+++ precache-manifest.222a49c3c325944296d96af1bfcfc532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb21735a9d70f1aa9e97d1fd4df11c8",
+    "url": "/country-finder/index.html"
+  },
+  {
+    "revision": "6e947378588b7eea0f12",
+    "url": "/country-finder/static/css/main.49e1fc0d.chunk.css"
+  },
+  {
+    "revision": "ef416c0016a2f5bad4cc",
+    "url": "/country-finder/static/js/2.a3dcf6a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-finder/static/js/2.a3dcf6a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e947378588b7eea0f12",
+    "url": "/country-finder/static/js/main.b88a7e15.chunk.js"
+  },
+  {
+    "revision": "3f5e14f45d880a142a62",
+    "url": "/country-finder/static/js/runtime-main.bac9da37.js"
+  }
+]);
